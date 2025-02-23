46333568a40e2602ff87a0cd457e9651a6042557
israelopeters <israelopeters@gmail.com>
2025-02-23 20:39:47 +0000

    Initial local comit

---
+++ src/main/java/com/example/bookieboard_backend/BookieboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookieboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookieboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookieboardBackendApplication.class, args);
+	}
+
+}
