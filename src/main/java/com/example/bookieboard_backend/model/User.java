2dbd619f2df8366d761f17e18b9e3baa5661d1b3
israelopeters <israelopeters@gmail.com>
2025-02-25 20:31:18 +0000

    feat: Create user model entity class

---
+++ src/main/java/com/example/bookieboard_backend/model/User.java
@@ -0,0 +1,55 @@
+package com.example.bookieboard_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    private LocalDate dateCreated;
+
+    private UserRank bookieRank;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = { @JoinColumn(name = "user_id", referencedColumnName = "id") },
+            inverseJoinColumns = { @JoinColumn(name = "role_id", referencedColumnName = "id") }
+    )
+    private List<Role> roles =  new ArrayList<>();
+
+    enum UserRank {
+        ROOKIE,
+        SEASONED,
+        VETERAN
+    }
+
+}
