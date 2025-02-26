540c31f3de7248223b85642ecd7ced84e7033a7a
israelopeters <israelopeters@gmail.com>
2025-02-26 07:53:48 +0000

    feat: Configure web security

---
+++ src/main/java/com/example/bookieboard_backend/config/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.bookieboard_backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(
+                                "/api/v1/*"
+                        ).permitAll()
+                        .requestMatchers("/api/v1/users/delete*").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .securityContext((securityContext) -> securityContext
+                        .requireExplicitSave(true)
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    // NOTE: Autowiring or setting this method as a bean creates a circular dependency problem that crashes the app
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+
+        auth.eraseCredentials(false);
+    }
+}
---
+++ src/main/java/com/example/bookieboard_backend/service/CustomUserDetailService.java
@@ -0,0 +1,51 @@
+package com.example.bookieboard_backend.service;
+
+import com.example.bookieboard_backend.model.Role;
+import com.example.bookieboard_backend.model.User;
+import com.example.bookieboard_backend.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Optional<User> userCheck = userRepository.findByEmail(email);
+
+        if (userCheck.isEmpty()) {
+            throw new UsernameNotFoundException("Invalid username or password");
+        } else {
+            User user = userCheck.get();
+            return new org.springframework.security.core.userdetails.User(
+                    user.getEmail(),
+                    user.getPassword(),
+                    mapRolesToAuthorities(user.getRoles())
+            );
+        }
+    }
+
+    private Collection< ? extends GrantedAuthority> mapRolesToAuthorities(Collection <Role> roles) {
+        return roles
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+        // TODO: Based on Lines 43 and 46, refactor roles class name field to follow Spring Security conventions
+    }
+}
