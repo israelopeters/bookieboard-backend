e181dcd2b76636581401573103428e78dce0edb5
israelopeters <israelopeters@gmail.com>
2025-02-27 18:12:07 +0000

    feat: Add custom query to method for getting questions by difficulty level in repository

--- src/main/java/com/example/bookieboard_backend/repository/QuestionRepository.java
+++ src/main/java/com/example/bookieboard_backend/repository/QuestionRepository.java
@@ -3,11 +3,14 @@ package com.example.bookieboard_backend.repository;
 import com.example.bookieboard_backend.model.Question;
 import com.example.bookieboard_backend.model.QuestionDifficultyLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    @Query("select q from Question q where q.difficultyLevel = ?1")
     List<Question> findByDifficultyLevel(QuestionDifficultyLevel difficultyLevel);
 }
