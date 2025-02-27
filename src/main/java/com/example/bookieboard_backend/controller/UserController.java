7a2db2acfeafd1a4e867e926d8c811899b2cf0e5
israelopeters <israelopeters@gmail.com>
2025-02-27 05:41:14 +0000

    feat: Add endpoint to add a new user in controller

--- src/main/java/com/example/bookieboard_backend/controller/UserController.java
+++ src/main/java/com/example/bookieboard_backend/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.example.bookieboard_backend.controller;
 
 import com.example.bookieboard_backend.model.User;
+import com.example.bookieboard_backend.model.dto.UserCreationDto;
 import com.example.bookieboard_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +25,12 @@ public class UserController {
     @GetMapping("/email")
     public ResponseEntity<User> getUserByEmail(@RequestParam String email) {
         return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody UserCreationDto userCreationDto) {
+        return new ResponseEntity<>(
+                userService.addUser(userCreationDto), HttpStatus.CREATED);
     }
 
 }
