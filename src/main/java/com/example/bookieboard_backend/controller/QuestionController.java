8eb80418bb3c8efc59214313a75855d4fe74fc2e
israelopeters <israelopeters@gmail.com>
2025-02-27 18:43:33 +0000

    feat: Create endpoint to add a new question in controller

--- src/main/java/com/example/bookieboard_backend/controller/QuestionController.java
+++ src/main/java/com/example/bookieboard_backend/controller/QuestionController.java
@@ -6,10 +6,7 @@ import com.example.bookieboard_backend.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +29,11 @@ public class QuestionController {
 
         return new ResponseEntity<>(
                 questionService.getQuestionsByDifficultyLevel(difficultyLevel), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Question> addQuestion(@RequestBody Question question) {
+        return new ResponseEntity<>(
+                questionService.addQuestion(question), HttpStatus.CREATED);
     }
 }
