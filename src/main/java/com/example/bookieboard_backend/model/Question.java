44e2d9e89a7e7c56cca4aa7c9918a11e0164c542
israelopeters <israelopeters@gmail.com>
2025-02-27 17:32:09 +0000

    feat: Create Question model

---
+++ src/main/java/com/example/bookieboard_backend/model/Question.java
@@ -0,0 +1,34 @@
+package com.example.bookieboard_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "questions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String detail;
+
+    private List<String> options = new ArrayList<>();
+
+    private DifficultyLevel difficultyLevel;
+
+    public enum DifficultyLevel {
+        EASY,
+        MEDIUM,
+        HARD
+    }
+}
