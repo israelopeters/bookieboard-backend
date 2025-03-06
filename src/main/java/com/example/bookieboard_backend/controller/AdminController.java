e076334b2610219c09437d00907af12b467c7976
israelopeters <israelopeters@gmail.com>
2025-03-06 23:46:52 +0000

    feat: Create non-RESTful controller for admin functionality

--- src/main/java/com/example/bookieboard_backend/controller/AdminController.java
+++ src/main/java/com/example/bookieboard_backend/controller/AdminController.java
@@ -1,15 +1,46 @@
 package com.example.bookieboard_backend.controller;
 
+import com.example.bookieboard_backend.model.Question;
+import com.example.bookieboard_backend.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class AdminController {
 
+    @Autowired
+    QuestionService questionService;
 
-    // Admin login
 
-    // Admin logout
+    @GetMapping("/questions")
+    public String reviews(Model model) {
+        List<Question> questionList = new ArrayList<>(questionService.getAllQuestions());
+        model.addAttribute("questionList", questionList);
+        return "questions";
+    }
 
-    // Add new question
+    @GetMapping("questions/add")
+    public String showNewQuestionForm(Model model) {
+        Question question = new Question();
+        model.addAttribute("newQuestion", question);
+        return "add_question";
+    }
 
+    @PostMapping("/questions/add/save")
+    public String addNewQuestion(
+            @ModelAttribute("newQuestion") Question question, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return "Error";
+        }
+        questionService.addQuestion(question);
+        return "redirect:/questions"; // Redirect to endpoint instead of template so as to load updated reviews list
+    }
 }
